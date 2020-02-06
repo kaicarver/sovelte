17de6abbe636f10d70517c468bf270989e173b58
Kai Carver <kaicarver@gmail.com>
2020-02-06 23:22:36 +0100

    add more stuff from tutorial

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Thing from './Thing.svelte';
+
   export let name;
   let count = 0;
   $: doubled = count * 2;
@@ -6,6 +8,18 @@
   function handleClick() {
     count += 1;
   }
+
+	let things = [
+		{ id: 1, color: '#0d0887' },
+		{ id: 2, color: '#6a00a8' },
+		{ id: 3, color: '#b12a90' },
+		{ id: 4, color: '#e16462' },
+		{ id: 5, color: '#fca636' }
+	];
+
+	function handleClick2() {
+		things = things.slice(1);
+	}
 </script>
 
 <style>
@@ -21,5 +35,13 @@
 </button>
 
 <p>{count} doubled is {doubled}</p>
+
+<button on:click={handleClick2}>
+	Remove first thing
+</button>
+
+{#each things as thing}
+	<Thing current={thing.color}/>
+{/each}
 
 <p>This rocks.</p>
