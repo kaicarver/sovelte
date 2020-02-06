b15bcc77cd719c6ddada506f11ed005b83502e26
Kai Carver <kaicarver@gmail.com>
2020-02-06 03:21:20 +0100

    a reactive button

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,22 @@
 <script>
-	export let name;
+  export let name;
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
 </script>
 
 <style>
-	h1 {
-		color: green;
-	}
+  h1 {
+  color: green;
+  }
 </style>
 
 <h1>Hey {name}!</h1>
+
+<button on:click={handleClick}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
 
 <p>This rocks.</p>
