3e6999a12dd3a7f3d455abcb260e66db2a426fab
Kai Carver <kaicarver@gmail.com>
2020-02-06 03:30:35 +0100

    a bit more reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   export let name;
   let count = 0;
+  $: doubled = count * 2;
 
   function handleClick() {
     count += 1;
@@ -18,5 +19,7 @@
 <button on:click={handleClick}>
   Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
+
+<p>{count} doubled is {doubled}</p>
 
 <p>This rocks.</p>
