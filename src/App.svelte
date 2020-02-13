734475d6a41b45e2d29e9764c68abf34dbd493ca
Kai Carver <kaicarver@gmail.com>
2020-02-13 08:31:52 +0100

    credit Periodic Table JSON

--- src/App.svelte
+++ src/App.svelte
@@ -57,5 +57,6 @@
 <p>This rocks.</p>
 
 <a href="https://github.com/kaicarver/sovelte">Source on Github</a> /
-<a href="https://zh.wikipedia.org/wiki/%E5%85%83%E7%B4%A0%E5%91%A8%E6%9C%9F%E8%A1%A8#%E5%85%83%E7%B4%A0%E9%80%B1%E6%9C%9F%E8%A1%A8">Periodic Table in Chinese on Wikipedia</a>
+<a href="https://zh.wikipedia.org/wiki/%E5%85%83%E7%B4%A0%E5%91%A8%E6%9C%9F%E8%A1%A8#%E5%85%83%E7%B4%A0%E9%80%B1%E6%9C%9F%E8%A1%A8">Periodic Table in Chinese on Wikipedia</a> /
+<a href="https://github.com/Bowserinator/Periodic-Table-JSON#periodic-table-json">Periodic Table JSON by Bowserinator</a>
 
